5fd617a9fc147872c52c6abd9683952aec000972
Yourbandit1 <98297682+Yourbandit1@users.noreply.github.com>
2022-03-20 13:42:09 +0600

    Update config.java

--- Android_Code/app/src/main/java/com/example/reverseshell2/config.java
+++ Android_Code/app/src/main/java/com/example/reverseshell2/config.java
@@ -1,7 +1,7 @@
 package com.example.reverseshell2;
 
 public class config {
-    public static String IP = "192.168.0.105";
+    public static String IP = "127.0.0.1";
     public static String port = "8888";
     public static boolean icon = true;
 }
